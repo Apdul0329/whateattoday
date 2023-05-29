80d4ede618039ce1642816be42e20a848a898fae
apdul <shrmsdn2067@gmail.com>
2023-05-30 00:44:10 +0900

    STYLE: Test for jenkins setting
    Test for jenkins setting

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,6 @@ const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
     target: 'node',
-    nodeExternals: [nodeExternals()],
-    entry: './',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-    },
-}
+    externals: [nodeExternals()],
+    entry: './'
+  }
