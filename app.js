696b25c9018e5cab8628f4cb711cd99e9acfe6bd
apdul <shrmsdn2067@gmail.com>
2023-05-31 23:01:25 +0900

    FEAT: Feat default setting for development
    Feat middleware for resolving cors problem
    Feat middleware for using swagger to show API
    Import dotenv package for environmental variables

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-import express from 'express';
+//express 모듈을 사용
+import express, { json, urlencoded } from 'express';
+import cors from 'cors';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
+import options from './config/swagger.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello, Jenkins')
-})
+const port = process.env.SERVER_PORT || 8000;
 
-app.listen(8000)
+// 'body-parser' 모듈을 사용해 url로 넘어 오는 값을 JSON 형식으로 바꾸는 미들웨어 사용
+app.use(json());
+app.use(urlencoded({extended: false}));
+
+app.use(cors({
+    origin: '*',
+    credentials: true
+}));
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJSDoc(options), {explorer: true}));
+
+app.listen(port, () => {
+    console.log('start server');
+});
