d7e1a6b9cf809ae79150e98bd06ab46c154bab99
apdul <shrmsdn2067@gmail.com>
2023-06-01 20:14:37 +0900

    STYLE: Test CI/CD
    Test CI/CD

--- app.js
+++ app.js
@@ -17,10 +17,7 @@ const app = express()
 
 const port = process.env.SERVER_PORT || 8000;
 
-app.use(cors({
-    origin: '*',
-    credentials: true
-}));
+app.use(cors());
 
 /*
 * 세션을 사용하기 위해 'express-session' 모듈을 session에 할당하고 미들웨어 사용
